34ce006979a657625ac901d7541e383312f14d4d
Vincenzo Pacella <shaduz@shaduzlabs.com>
2015-10-01 08:50:46 +0200

    removed submodules, added catch as external project, added sl-cmake files

--- cmake-modules/sl-main.cmake
+++ cmake-modules/sl-main.cmake
@@ -1,9 +1,9 @@
-/*
+
         ##########    Copyright (C) 2015 Vincenzo Pacella
         ##      ##    Distributed under MIT license, see file LICENSE
         ##      ##    or <http://opensource.org/licenses/MIT>
         ##      ##
-##########      ############################################################# shaduzlabs.com #####*/
+##########      ############################################################# shaduzlabs.com #######
 
-#define CATCH_CONFIG_MAIN
-#include <catch.hpp>
+include(sl-compiler)
+include(sl-ext-libs)
